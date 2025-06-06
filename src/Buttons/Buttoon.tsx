b046921b211aa36eb4a7fa2a23fa7d22c580e804
Saiful Islam <76849761+saifulIslamSourceCode@users.noreply.github.com>
2025-06-06 06:26:10 +0600

    inline styling in a component Button

--- src/Buttons/Buttoon.tsx
+++ src/Buttons/Buttoon.tsx
@@ -1,10 +1,22 @@
 import { type JSX } from "react";
-import style from './Button.module.css'
+// import style from './Button.module.css'
+
 
 const Button = (): JSX.Element => {
-    return(
-        <button className={style.button}>Click Me</button>
-    )
+    //inline style using object
+    const styles = {
+            padding: "15px 20px",
+            borderRadius: "5px",
+            margin: "5px",
+            color: "white",
+            backgroundColor: "rgb(0, 100, 158)",
+            border: "0px",
+            cursor: "pointer",
+    }
+
+return (
+    <button style={styles}>Click Me</button>
+)
 }
 
 export default Button;
